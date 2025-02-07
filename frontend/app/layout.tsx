76e096e427702d27f421ee2b7b92d9423c460399
lokytech5 <lokytech5@gmail.com>
2025-02-07 21:36:26 +0000

    set up react toast

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "./provider";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <Providers>
         <main>{children}</main>
+        <ToastContainer/>
           </Providers>
       </body>
     </html>
