7e927d16831ccc6555a024e32ec8ac594ea7a83e
lokytech5 <lokytech5@gmail.com>
2025-02-12 09:48:25 +0000

    fix bug issue related to starting the container in docker

--- backend/db.ts
+++ backend/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost/playDB");
+        await mongoose.connect("mongodb://mongo-db:27017/playDB?authSource=admin&replicaSet=rs0");
         console.log('Connected to MongoDB...');
     } catch (error) {
         console.log("Could not connect to Mongodb", error);
