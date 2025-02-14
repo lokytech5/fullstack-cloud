d490a0f362c341b4a3630c99a6a3391df8bcbf2c
lokytech5 <lokytech5@gmail.com>
2025-02-14 21:09:52 +0000

    yet another update

--- backend/index.ts
+++ backend/index.ts
@@ -1,20 +1,22 @@
 import express from 'express';
-
 import connectDB from './db';
 import { articlesRouter } from './routers/articlesRouter';
-
-import cors from "cors"
+import cors from "cors";
 
 const app = express();
 
+// Allow requests from frontend domain
 app.use(cors({
-    origin: ["http://fullstack.dostech.solutions", "http://full-stack-ALB-337628920.us-east-1.elb.amazonaws.com"],
-    allowedHeaders: ["Content-Type", "Authorization", "Accept"]
+    origin: ["http://fullstack.dostech.solutions"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    optionsSuccessStatus: 204
 }));
-
 
 app.use(express.json());
 app.use('/api/articles', articlesRouter);
 
-app.listen(8000, "0.0.0.0",() => console.log(`listening to port ${8000}`));
+// Ensure backend listens on all interfaces
+app.listen(8000, "0.0.0.0", () => console.log("Backend running on port 8000"));
+
 connectDB();
